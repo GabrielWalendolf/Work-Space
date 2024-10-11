a324a336ea44a8c6ec35fab7f3fea80b21e5394f
Gabriel Walendolf <gabriel.g.walendolf@gmail.com>
2024-10-11 20:20:07 -0300

    dia11_10_2024

--- Codewars/testes.py
+++ Codewars/testes.py
@@ -36,4 +36,16 @@ def row_sum_odd_numbers(n):
     return sum(piramide[-1])
 print(row_sum_odd_numbers(n))
 """
-
+names = ['Peter']
+def likes(names):
+    if len(names) == 0:
+        return 'no one likes this'
+    elif len(names) == 1:
+        return (f'{names[0]} like this')
+    elif len(names) == 2:
+        return (f'{names[0]} and {names[1]} like this')
+    elif len(names) == 3:
+        return (f'{names[0]}, {names[1]} and {names[2]} like this')
+    else:
+        return (f'{names[0]}, {names[1]} and {len(names)-2} others like this')
+print(likes(names))
