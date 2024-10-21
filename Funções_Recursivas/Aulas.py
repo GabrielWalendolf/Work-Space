7837a4412ba3971c8a975aac2d4b36320a19318f
Gabriel Walendolf <gabriel.g.walendolf@gmail.com>
2024-10-21 19:39:22 -0300

    Função Recursiva

---
+++ Funções_Recursivas/Aulas.py
@@ -0,0 +1,24 @@
+#aula 1
+"""
+#exemplo de função recursiva utilizando fatorial
+#funções recursivas tem que ter obrigatóriamente um caso base e um caso recursivo. Caso o caso base seja executado, o caso recursivo não será executado.
+def fatorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * fatorial(n-1)
+
+print(fatorial(5))
+"""
+"""
+#exemplo de função recursiva utilizando fibonacci
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+print(fibonacci(6))
+"""
