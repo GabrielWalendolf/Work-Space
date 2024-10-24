016f1dbc90a036b3727b5e8d0bf8f2e7f20272ea
Gabriel Walendolf <gabriel.g.walendolf@gmail.com>
2024-10-24 02:43:06 -0300

    testes

--- sites/Portifólio/custom.js
+++ sites/Portifólio/custom.js
@@ -1,19 +1,1 @@
-console.log("Gabriel Walendolf");
 
-let carro = new Object();
-carro.marca = "Fiat";
-carro.cor = "Azul";
-carro.ano = "2000";
-console.log(carro);
-
-document.getElementById("carro").value = carro.marca;
-document.getElementById("cor").value = carro.cor;
-document.getElementById("ano").value = carro.ano;
-
-carregarDados = () => {
-    let carro = new Object();
-    carro.marca = document.getElementById("carro").value;
-    carro.cor = document.getElementById("cor").value;
-    carro.ano = document.getElementById("ano").value;
-    console.log("Carro: " + carro.marca + ", cor: " + carro.cor + ", ano: " + carro.ano);
-}
