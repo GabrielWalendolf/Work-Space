1371bc17b9e336516d8bc30b83ada74125c59fef
Gabriel Walendolf <gabriel.g.walendolf@gmail.com>
2024-11-06 03:16:13 -0300

    Implementa matriz 2x4 com input do usuário (Questão 04)

---
+++ PYTHON/test/prova.py
@@ -0,0 +1,16 @@
+
+#NOTE:Questão 03
+"""
+def
+"""
+#NOTE:Questão 04 crie uma matriz 2x4 que peça para o usuário informar os valores desejados na matriz e imprima a matriz
+"""
+mat=[]
+for i in range(2):
+  aux=[]
+  for j in range(4):
+    x=int(input('Digite um número'))
+    aux.append(x)
+  mat.append(aux)
+print(mat)
+"""
